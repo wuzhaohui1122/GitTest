e70d28840d94a1b9e78e542b5abcee5a9e6bdadd
wuzhaohui1122 <wuzhaohui18729371641@hotmail.com>
2016-05-02 22:19:12 +0800

    Update Hello1122.java

--- src/com/atguigu/com/Hello1122.java
+++ src/com/atguigu/com/Hello1122.java
@@ -3,8 +3,9 @@ package com.atguigu.com;
 public class Hello1122 {
 
 	public static void main(String[] args) {
-      System.out.println("github hello 01,第一次request");
+          System.out.println("github hello 01,第一次request");
 	  System.out.println("github hello 02,第二次request");
+	  System.out.pintln("收到第一次回复....");
 	}
 
 }
