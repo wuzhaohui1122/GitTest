97f1008fc6a3a09faf5d9eba3e69e29888f9283a
wuzhaohui1122 <wuzhaohui18729371641@hotmail.com>
2016-05-02 22:12:40 +0800

    第二次请求

--- src/com/atguigu/com/Hello1122.java
+++ src/com/atguigu/com/Hello1122.java
@@ -4,7 +4,7 @@ public class Hello1122 {
 
 	public static void main(String[] args) {
       System.out.println("github hello 01,第一次request");
-		
+	  System.out.println("github hello 02,第二次request");
 	}
 
 }
